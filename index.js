769b4a8652c3c7750a1dd81ffda1c75632660a0e
LucasJorgeHubert <lucasjhubert@gmail.com>
2018-04-17 10:10:56 -0300

    Instalação

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine', 'njk');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) =>{
+    res.render('index', { nome: 'Lucas Hubert' });
+});
+
+app.listen(3000);
