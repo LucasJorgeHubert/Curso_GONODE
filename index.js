9e3361b51f02bf076655bbb029133dd21ca047e7
LucasJorgeHubert <lucasjhubert@gmail.com>
2018-04-17 12:13:00 -0300

    Desafio 01 GoNode
    Este commit trás a solução do desafio 01 GoNode.
    
    Verificador de maior idade penal:
    
    Com npm de moment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const path = require('path');
 const bodyParser = require('body-parser');
+const moment = require('moment');
 
 const app = express();
 
@@ -14,8 +15,35 @@ app.set('view engine', 'njk');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+
 app.get('/', (req, res) =>{
-    res.render('index', { nome: 'Lucas Hubert' });
+    res.render('main');
+});
+
+app.post('/check', (req,res) =>{
+  var a = moment(new Date());
+  var b = moment(req.body.age);
+  if (a.diff(b, 'years') >= 18) {
+    res.render('major', { nome : req.body.username });
+  }else{
+    res.render('minor', { nome : req.body.username });
+  }
+});
+
+const verificaPreenchimento = ((req,res,next)=> {
+  const user = req.body.username;
+  const age = req.body.age;
+  if (!user & !age){
+    res.render('main');
+  }
+});
+
+app.get('/major', verificaPreenchimento, (req, res) => {
+  res.render('major');
+});
+
+app.get('/minor', verificaPreenchimento, (req, res) => {
+  res.render('minor');
 });
 
 app.listen(3000);
